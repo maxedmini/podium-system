d42ebb786e43192cc25807ded3ec6b437fc1d265
maxedmini <35852995+maxedmini@users.noreply.github.com>
2025-12-23 04:24:30 +0000

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -35,7 +35,7 @@ echo "Podium: $PODIUM"
 # Packages
 sudo apt update
 sudo apt install -y \
-  git curl rsync sshpass \
+  git curl rsync sshpass openssh-client \
   chromium x11-xserver-utils unclutter \
   python3 python3-venv python3-pip
 
